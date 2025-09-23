eba7974d6339270c37c097fb4a93985ec83091f4
everett carpenter <65781323+SaintEverett@users.noreply.github.com>
2025-09-23 14:04:12 -0400

    stuf

--- Spherical-Harmonics.cpp
+++ Spherical-Harmonics.cpp
@@ -1,10 +1,14 @@
-// Online C++ compiler to run C++ program online
+
 #include <iostream>
 #include <cmath>
 #include <complex>
 #include <vector>
 
 #define pi 3.14159265358979323846264
+#define fourpi 12.566370614359172953850
+#define sqrt4pi  3.544907701811032 // iem
+
+constexpr float decodeCorrection(const int N) { return sqrt4pi / (N + 1) / (N + 1); } // iem
 
 float rad2degree(float radians)
 {
@@ -23,38 +27,51 @@ constexpr int factorial(int n)
 
 float norme(unsigned order, int degree)
 {
-    float l_frac = ((2.f * order + 1) / (4.f * pi));
-    float t_r = factorial(order - abs(degree));
-    float b_r = factorial(order + abs(degree));
-    return sqrtf(l_frac * (t_r / b_r));
+    float lf = (2.f * (int)order) + 1;
+    float t = factorial((int)order - abs(degree));
+    float b = factorial((int)order + abs(degree));
+    float res = (lf / fourpi) * (t / b);
+    // std::cout << "Order: " << order << " Degree: " << degree << " Norm: " << (sqrtf(res)) << std::endl;
+    return (sqrtf(res));
+}
+
+float norme(int order, int degree, bool n3d)
+{
+    int d = (degree == 0) ? 1 : 0;  // Kronecker delta
+    float ratio = static_cast<float>(factorial(order - abs(degree))) / factorial(order + abs(degree));
+    return n3d ? sqrtf(2 * order + 1) * sqrtf((2.f - d) / (4.f * pi) * ratio) : sqrtf((2.f - d) / (4.f * pi) * ratio); // n3d = TRUE? then N3D (equal weighting),,,, n3d = FALSE? then SN3D
 }
 
 float inversenorme(unsigned order, int degree)
 {
-    float t_r = factorial(order - abs(degree));
-    float b_r = factorial(order + abs(degree));
-    return sqrt(4*pi) * sqrtf(b_r / t_r);
+    float lf = (2.f * (int)order) + 1;
+    float t = factorial((int)order - abs(degree));
+    float b = factorial((int)order + abs(degree));
+    float res = (lf / fourpi) * (t / b);
+    //std::cout << "Order: " << order << " Degree: " << degree << " Inverse Norm: " << (sqrtf(res)) << std::endl;
+    return (1.f / (sqrtf(res)));
 }
 
 std::vector<float> SH(unsigned order_, const float azimuth_, const float zenith_)
 {
     float azimuth = degree2rad(azimuth_);
     float zenith = degree2rad(zenith_);
-    std::vector<float> result = std::vector<float>((int)pow((order_ + 1), 2), 0); // instantiate and reserve a vector that is the size of the results that shall be returned
-    for (int order = 0; order <= order_; order++)
+    std::vector<float> result = std::vector<float>(pow(((int)order_ + 1), 2), 0); // instantiate and reserve a vector that is the size of the results that shall be returned
+    for (int order = 0; order <= (int)order_; order++)
     {
-        if (order == 0) result[0] = norme(order, 0);
+        if (order == 0) result[0] = norme(order, 0, 0);
         for (int i = -order; i <= order; i++)
         {
-            float n = norme(order, i);
-            float p = (std::assoc_legendref(order, abs(i), cosf((pi / 2.f) - zenith))); // subtract 90 degrees to rotate
+            float n = norme(order, i, 0);
+            float p = (std::assoc_legendref(order, abs(i), cosf(zenith))); 
             float r = 0.f;
-            if (i < 0) r = sinf(i * ((pi / 2.f) - azimuth)); // subtract 90 degrees to rotate
-            else if (i >= 0) r = cosf(i * ((pi / 2.f) - azimuth)); // subtract 90 degrees to rotate
-            std::cout << "Order: " << order << " Degree: " << i << " Complex component: " << r << " Legendre: " << p << " Normalization term: " << n << " ACN: " << (pow(order, 2) + order + i) << '\n';
+            if (i < 0) r = sinf(abs(i) * (azimuth)); 
+            else if (i >= 0) r = cosf(i * (azimuth)); 
+            // std::cout << "Order: " << order << " Degree: " << i << " Complex component: " << r << " Legendre: " << p << " Normalization term: " << n << " ACN: " << (pow(order, 2) + order + i) << '\n';
             result[pow(order, 2) + order + i] = (n * p * r); // place inside vector so it is ordered as Y^0_0, Y^1_-1, Y^1_0, Y^1_1
         }
     }
+
     return result;
 }
 
@@ -65,10 +82,14 @@ std::vector<float> BFormSH(std::vector<float> SH)
     int degree;
     for (int ACN = 0; ACN < SH.capacity(); ACN++)
     {
-        order = floor(sqrtf(ACN));
-        degree = ACN - pow(order, 2) - order;
-        std::cout << "Order: " << order << " Degree: " << degree << " Encode factor: " << inversenorme(order, degree) << '\n';
-        result[ACN] = SH[ACN] * inversenorme(order, degree);
+        int order = 0;
+        while ((order + 1) * (order + 1) <= ACN) 
+        {
+            order++;
+        }
+        int degree = ACN - (order * order) - order;
+        //std::cout << "Order: " << order << " Degree: " << degree << " Encode factor: " << inversenorme(order, degree) << '\n';
+        result[ACN] = SH[ACN];
     }
     return result;
 }
@@ -76,19 +97,27 @@ std::vector<float> BFormSH(std::vector<float> SH)
 
 int main() 
 {
-    float azi = 45.f;
-    float zeni = 26.f;
-    unsigned order = 4;
+    float azi = 42.f;
+    float zeni = 42.f;
+    unsigned order = 6;
+    int degree;
     std::vector<float> spheric = SH(order, azi, zeni);
     for (int i = 0; i < spheric.capacity(); i++)
     {
-        std::cout << "Raw: " << spheric[i] << '\n';
+        int order = 0;
+        while ((order + 1) * (order + 1) <= i) { order++; }
+        int degree = i - (order * order) - order;
+        std::cout << "ACN: " << i << " Order: " << order << " Degree: " << degree << " Normalization: " << norme(order, degree, 0) << '\n';
+        //std::cout << "Raw: " << spheric[i] << '\n';
     }
     std::cout << std::endl;
     std::vector<float> bformat = BFormSH(spheric);
     for (int i = 0; i < bformat.capacity(); i++)
     {
-        std::cout << "B-Format: " << bformat[i] << '\n';
+        int order = 0;
+        while ((order + 1) * (order + 1) <= i) { order++; }
+        int degree = i - (order * order) - order;
+        std::cout << "ACN: " << i << " B-Format: " << bformat[i] << '\n';
     }
     return 0;
 }
